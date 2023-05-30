fba44b8dafff8bf2b73c1db75561c08d516db29a
Alex Rutherford <am-rutherford@robots.ox.ac.uk>
2023-05-30 16:37:02 +0100

    improving registration

--- multiagentgymnax/registration.py
+++ multiagentgymnax/registration.py
@@ -17,40 +17,41 @@ def make(env_id: str, **env_kwargs):
         raise ValueError(f"{env_id} is not in registered SMAX environments.")
     
     # 1. MPE PettingZoo Environments
-    if env_id == "simple_v2":
-        return SimpleMPE(**env_kwargs)
-    elif env_id == "simple_tag_v2":
-        return SimpleTagMPE(**env_kwargs)
-    elif env_id == "simple_world_comm_v2":
-        return SimpleWorldCommMPE(**env_kwargs)
-    elif env_id == "simple_spread_v2":
-        return SimpleSpreadMPE(**env_kwargs)
-    elif env_id == "simple_crypto_v2":
-        return SimpleCryptoMPE(**env_kwargs)
-    elif env_id == "simple_speaker_listener_v3":
-        return SimpleSpeakerListenerMPE(**env_kwargs)
-    elif env_id == "simple_push_v2":
-        return SimplePushMPE(**env_kwargs)
-    elif env_id == "simple_adversary_v2":
-        return SimpleAdversaryMPE(**env_kwargs)
-    elif env_id == "simple_reference_v2":
-        return SimpleReferenceMPE(**env_kwargs)
+    if env_id == "MPE_simple_v2":
+        env = SimpleMPE(**env_kwargs)
+    elif env_id == "MPE_simple_tag_v2":
+        env = SimpleTagMPE(**env_kwargs)
+    elif env_id == "MPE_simple_world_comm_v2":
+        env = SimpleWorldCommMPE(**env_kwargs)
+    elif env_id == "MPE_simple_spread_v2":
+        env = SimpleSpreadMPE(**env_kwargs)
+    elif env_id == "MPE_simple_crypto_v2":
+        env = SimpleCryptoMPE(**env_kwargs)
+    elif env_id == "MPE_simple_speaker_listener_v3":
+        env = SimpleSpeakerListenerMPE(**env_kwargs)
+    elif env_id == "MPE_simple_push_v2":
+        env = SimplePushMPE(**env_kwargs)
+    elif env_id == "MPE_simple_adversary_v2":
+        env = SimpleAdversaryMPE(**env_kwargs)
+    elif env_id == "MPE_simple_reference_v2":
+        env = SimpleReferenceMPE(**env_kwargs)
     
     # 2. Switch Riddle
     elif env_id == "switch_riddle":
-        return SwitchRiddle(**env_kwargs)
+        env = SwitchRiddle(**env_kwargs)
     
+    return env, env.default_params
     
 registered_envs = [
-    "simple_v2",
-    "simple_tag_v2",
-    "simple_world_comm_v2",
-    "simple_spread_v2",
-    "simple_crypto_v2",
-    "simple_speaker_listener_v3",
-    "simple_push_v2",
-    "simple_adversary_v2",
-    "simple_reference_v2",
+    "MPE_simple_v2",
+    "MPE_simple_tag_v2",
+    "MPE_simple_world_comm_v2",
+    "MPE_simple_spread_v2",
+    "MPE_simple_crypto_v2",
+    "MPE_simple_speaker_listener_v3",
+    "MPE_simple_push_v2",
+    "MPE_simple_adversary_v2",
+    "MPE_simple_reference_v2",
     "switch_riddle",
 ]
     
