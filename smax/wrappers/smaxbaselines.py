525395abc4ebc87ed752023680b146e1c3166b0b
benellis3 <benellis@robots.ox.ac.uk>
2023-10-03 14:45:56 +0100

    Fix LogWrapper

--- smax/wrappers/smaxbaselines.py
+++ smax/wrappers/smaxbaselines.py
@@ -89,7 +89,7 @@ class LogWrapper(SMAXWrapper):
 
 
 @struct.dataclass
-class LogEnvState:
+class SMAXLogEnvState:
     env_state: State
     episode_returns: float
     episode_lengths: int
@@ -107,7 +107,7 @@ class SMAXLogWrapper(SMAXWrapper):
     @partial(jax.jit, static_argnums=(0,))
     def reset(self, key: chex.PRNGKey) -> Tuple[chex.Array, State]:
         obs, env_state = self._env.reset(key)
-        state = LogEnvState(
+        state = SMAXLogEnvState(
             env_state,
             jnp.zeros((self._env.num_agents,)),
             jnp.zeros((self._env.num_agents,)),
@@ -132,7 +132,7 @@ class SMAXLogWrapper(SMAXWrapper):
         batch_reward = self._batchify_floats(reward)
         new_episode_return = state.episode_returns + self._batchify_floats(reward)
         new_episode_length = state.episode_lengths + 1
-        state = LogEnvState(
+        state = SMAXLogEnvState(
             env_state=env_state,
             episode_returns=new_episode_return * (1 - ep_done),
             episode_lengths=new_episode_length * (1 - ep_done),
