Fix win rate calculation and update smax logging

smax/wrappers/smaxbaselines.py
@@ -93,10 +93,10 @@ class SMAXLogEnvState:
     env_state: State
     episode_returns: float
     episode_lengths: int
+    won_episode: int
     returned_episode_returns: float
     returned_episode_lengths: int
-    won_episodes: int
-    total_episodes: int
+    returned_won_episode: int
 
 
 class SMAXLogWrapper(SMAXWrapper):
@@ -113,8 +113,8 @@ class SMAXLogWrapper(SMAXWrapper):
             jnp.zeros((self._env.num_agents,)),
             jnp.zeros((self._env.num_agents,)),
             jnp.zeros((self._env.num_agents,)),
-            won_episodes=jnp.zeros((self._env.num_agents,)),
-            total_episodes=jnp.zeros((self._env.num_agents,)),
+            jnp.zeros((self._env.num_agents,)),
+            jnp.zeros((self._env.num_agents,)),
         )
         return obs, state
 
@@ -122,7 +122,7 @@ class SMAXLogWrapper(SMAXWrapper):
     def step(
         self,
         key: chex.PRNGKey,
-        state: LogEnvState,
+        state: SMAXLogEnvState,
         action: Union[int, float],
     ) -> Tuple[chex.Array, LogEnvState, float, bool, dict]:
         obs, env_state, reward, done, info = self._env.step(
@@ -132,23 +132,23 @@ class SMAXLogWrapper(SMAXWrapper):
         batch_reward = self._batchify_floats(reward)
         new_episode_return = state.episode_returns + self._batchify_floats(reward)
         new_episode_length = state.episode_lengths + 1
+        new_won_episode = (batch_reward >= 1.0).astype(jnp.float32)
         state = SMAXLogEnvState(
             env_state=env_state,
+            won_episode=new_won_episode * (1 - ep_done),
             episode_returns=new_episode_return * (1 - ep_done),
             episode_lengths=new_episode_length * (1 - ep_done),
             returned_episode_returns=state.returned_episode_returns * (1 - ep_done)
             + new_episode_return * ep_done,
             returned_episode_lengths=state.returned_episode_lengths * (1 - ep_done)
             + new_episode_length * ep_done,
-            won_episodes=state.won_episodes + (batch_reward >= 1.0).astype(jnp.int32),
-            total_episodes=state.total_episodes + (ep_done).astype(jnp.int32),
+            returned_won_episode=state.returned_won_episode * (1 - ep_done)
+            + new_won_episode * ep_done,
         )
         if self.replace_info:
             info = {}
         info["returned_episode_returns"] = state.returned_episode_returns
         info["returned_episode_lengths"] = state.returned_episode_lengths
+        info["returned_won_episode"] = state.returned_won_episode
         info["returned_episode"] = jnp.full((self._env.num_agents,), ep_done)
-        info["win_rate"] = jnp.where(
-            state.total_episodes == 0, 0, state.won_episodes / state.total_episodes
-        )
         return obs, state, reward, done, info