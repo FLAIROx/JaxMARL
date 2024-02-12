949ea251f4018f553e79e1a8dec66763865e8f6c
mttga <matteogallici@yahoo.it>
2024-02-12 18:09:47 +0100

    remove utracking

--- jaxmarl/environments/utracking/traj_models/__init__.py
+++
@@ -1,8 +0,0 @@
-import json
-import os
-
-module_dir = os.path.dirname(__file__)
-json_file_path = os.path.join(module_dir, 'traj_linear_models.json')
-
-with open(json_file_path,'r') as f:
-    traj_models = json.load(f)
