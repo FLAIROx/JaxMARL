f081cb2d096ad0cb41e57a0f74c7b78f9653aff6
mttga <matteogallici@yahoo.it>
2024-03-28 22:00:26 +0100

    better params for ppo

--- baselines/IPPO/ippo_ff_hint_guess.py
+++ baselines/IPPO/ippo_ff_hint_guess.py
@@ -22,20 +22,27 @@ from omegaconf import OmegaConf
 
 class ActorCritic(nn.Module):
     action_dim: Sequence[int]
-    config: Dict
+    activation: str = "tanh"
+    hidden_size: int = 128
 
     @nn.compact
     def __call__(self, x):
+
+        if self.activation == "relu":
+            activation = nn.relu
+        else:
+            activation = nn.tanh
+
         obs, dones, avail_actions = x
         embedding = nn.Dense(
-            128, kernel_init=orthogonal(np.sqrt(2)), bias_init=constant(0.0)
+            self.hidden_size, kernel_init=orthogonal(np.sqrt(2)), bias_init=constant(0.0)
         )(obs)
-        embedding = nn.relu(embedding)
+        embedding = activation(embedding)
 
-        actor_mean = nn.Dense(128, kernel_init=orthogonal(2), bias_init=constant(0.0))(
+        actor_mean = nn.Dense(self.hidden_size, kernel_init=orthogonal(2), bias_init=constant(0.0))(
             embedding
         )
-        actor_mean = nn.relu(actor_mean)
+        actor_mean = activation(actor_mean)
         actor_mean = nn.Dense(
             self.action_dim, kernel_init=orthogonal(0.01), bias_init=constant(0.0)
         )(actor_mean)
@@ -43,10 +50,10 @@ class ActorCritic(nn.Module):
         action_logits = actor_mean - (unavail_actions * 1e10)
         pi = distrax.Categorical(logits=action_logits)
 
-        critic = nn.Dense(128, kernel_init=orthogonal(2), bias_init=constant(0.0))(
+        critic = nn.Dense(self.hidden_size, kernel_init=orthogonal(2), bias_init=constant(0.0))(
             embedding
         )
-        critic = nn.relu(critic)
+        critic = activation(critic)
         critic = nn.Dense(1, kernel_init=orthogonal(1.0), bias_init=constant(0.0))(
             critic
         )
@@ -64,41 +71,50 @@ class Transition(NamedTuple):
     info: jnp.ndarray
     avail_actions: jnp.ndarray
 
+
 def batchify(x: dict, agent_list, num_actors):
     x = jnp.stack([x[a] for a in agent_list])
     return x.reshape((num_actors, -1))
+
 
 def unbatchify(x: jnp.ndarray, agent_list, num_envs, num_actors):
     x = x.reshape((num_actors, num_envs, -1))
     return {a: x[i] for i, a in enumerate(agent_list)}
 
+
 def make_train(config):
     env = jaxmarl.make(config["ENV_NAME"], **config["ENV_KWARGS"])
     config["NUM_ACTORS"] = env.num_agents * config["NUM_ENVS"]
     config["NUM_UPDATES"] = (
-            config["TOTAL_TIMESTEPS"] // config["NUM_STEPS"] // config["NUM_ENVS"]
+        config["TOTAL_TIMESTEPS"] // config["NUM_STEPS"] // config["NUM_ENVS"]
     )
     config["MINIBATCH_SIZE"] = (
-            config["NUM_ACTORS"] * config["NUM_STEPS"] // config["NUM_MINIBATCHES"]
+        config["NUM_ACTORS"] * config["NUM_STEPS"] // config["NUM_MINIBATCHES"]
     )
 
     env = LogWrapper(env)
 
     def linear_schedule(count):
-        frac = 1.0 - (count // (config["NUM_MINIBATCHES"] * config["UPDATE_EPOCHS"])) / config["NUM_UPDATES"]
+        frac = (
+            1.0
+            - (count // (config["NUM_MINIBATCHES"] * config["UPDATE_EPOCHS"]))
+            / config["NUM_UPDATES"]
+        )
         return config["LR"] * frac
 
     def train(rng):
 
         # INIT NETWORK
-        network = ActorCritic(env.action_space(env.agents[0]).n, config=config)
+        network = ActorCritic(
+            action_dim=env.action_space(env.agents[0]).n,
+            activation=config["ACTIVATION"],
+            hidden_size=config["HIDDEN_SIZE"],
+        )
         rng, _rng = jax.random.split(rng)
         init_x = (
-            jnp.zeros(
-                (1, config["NUM_ENVS"], env.observation_space(env.agents[0]).n)
-            ),
+            jnp.zeros((1, config["NUM_ENVS"], env.observation_space(env.agents[0]).n)),
             jnp.zeros((1, config["NUM_ENVS"])),
-            jnp.zeros((1, config["NUM_ENVS"], env.action_space(env.agents[0]).n))
+            jnp.zeros((1, config["NUM_ENVS"], env.action_space(env.agents[0]).n)),
         )
         network_params = network.init(_rng, init_x)
         if config["ANNEAL_LR"]:
@@ -107,7 +123,10 @@ def make_train(config):
                 optax.adam(learning_rate=linear_schedule, eps=1e-5),
             )
         else:
-            tx = optax.chain(optax.clip_by_global_norm(config["MAX_GRAD_NORM"]), optax.adam(config["LR"], eps=1e-5))
+            tx = optax.chain(
+                optax.clip_by_global_norm(config["MAX_GRAD_NORM"]),
+                optax.adam(config["LR"], eps=1e-5),
+            )
         train_state = TrainState.create(
             apply_fn=network.apply,
             params=network_params,
@@ -123,6 +142,7 @@ def make_train(config):
         def _update_step(update_runner_state, unused):
             # COLLECT TRAJECTORIES
             runner_state, update_steps = update_runner_state
+
             def _env_step(runner_state, unused):
                 train_state, env_state, last_obs, last_done, rng = runner_state
 
@@ -133,19 +153,25 @@ def make_train(config):
                     batchify(avail_actions, env.agents, config["NUM_ACTORS"])
                 )
                 obs_batch = batchify(last_obs, env.agents, config["NUM_ACTORS"])
-                ac_in = (obs_batch[np.newaxis, :], last_done[np.newaxis, :], avail_actions[np.newaxis, :])
+                ac_in = (
+                    obs_batch[np.newaxis, :],
+                    last_done[np.newaxis, :],
+                    avail_actions[np.newaxis, :],
+                )
                 pi, value = network.apply(train_state.params, ac_in)
                 action = pi.sample(seed=_rng)
                 log_prob = pi.log_prob(action)
-                env_act = unbatchify(action, env.agents, config["NUM_ENVS"], env.num_agents)
+                env_act = unbatchify(
+                    action, env.agents, config["NUM_ENVS"], env.num_agents
+                )
                 env_act = jax.tree_map(lambda x: x.squeeze(), env_act)
 
                 # STEP ENV
                 rng, _rng = jax.random.split(rng)
                 rng_step = jax.random.split(_rng, config["NUM_ENVS"])
-                obsv, env_state, reward, done, info = jax.vmap(env.step, in_axes=(0, 0, 0))(
-                    rng_step, env_state, env_act
-                )
+                obsv, env_state, reward, done, info = jax.vmap(
+                    env.step, in_axes=(0, 0, 0)
+                )(rng_step, env_state, env_act)
                 info = jax.tree_map(lambda x: x.reshape((config["NUM_ACTORS"])), info)
                 done_batch = batchify(done, env.agents, config["NUM_ACTORS"]).squeeze()
                 transition = Transition(
@@ -156,7 +182,7 @@ def make_train(config):
                     log_prob.squeeze(),
                     obs_batch,
                     info,
-                    avail_actions
+                    avail_actions,
                 )
                 runner_state = (train_state, env_state, obsv, done_batch, rng)
                 return runner_state, transition
@@ -171,7 +197,11 @@ def make_train(config):
             avail_actions = jnp.ones(
                 (config["NUM_ACTORS"], env.action_space(env.agents[0]).n)
             )
-            ac_in = (last_obs_batch[np.newaxis, :], last_done[np.newaxis, :], avail_actions)
+            ac_in = (
+                last_obs_batch[np.newaxis, :],
+                last_done[np.newaxis, :],
+                avail_actions,
+            )
             _, last_val = network.apply(train_state.params, ac_in)
             last_val = last_val.squeeze()
 
@@ -185,8 +215,8 @@ def make_train(config):
                     )
                     delta = reward + config["GAMMA"] * next_value * (1 - done) - value
                     gae = (
-                            delta
-                            + config["GAMMA"] * config["GAE_LAMBDA"] * (1 - done) * gae
+                        delta
+                        + config["GAMMA"] * config["GAE_LAMBDA"] * (1 - done) * gae
                     )
                     return (gae, value), gae
 
@@ -208,18 +238,20 @@ def make_train(config):
 
                     def _loss_fn(params, traj_batch, gae, targets):
                         # RERUN NETWORK
-                        pi, value = network.apply(params,
-                                                  (traj_batch.obs, traj_batch.done, traj_batch.avail_actions))
+                        pi, value = network.apply(
+                            params,
+                            (traj_batch.obs, traj_batch.done, traj_batch.avail_actions),
+                        )
                         log_prob = pi.log_prob(traj_batch.action)
 
                         # CALCULATE VALUE LOSS
                         value_pred_clipped = traj_batch.value + (
-                                value - traj_batch.value
+                            value - traj_batch.value
                         ).clip(-config["CLIP_EPS"], config["CLIP_EPS"])
                         value_losses = jnp.square(value - targets)
                         value_losses_clipped = jnp.square(value_pred_clipped - targets)
                         value_loss = (
-                                0.5 * jnp.maximum(value_losses, value_losses_clipped).mean()
+                            0.5 * jnp.maximum(value_losses, value_losses_clipped).mean()
                         )
 
                         # CALCULATE ACTOR LOSS
@@ -227,21 +259,21 @@ def make_train(config):
                         gae = (gae - gae.mean()) / (gae.std() + 1e-8)
                         loss_actor1 = ratio * gae
                         loss_actor2 = (
-                                jnp.clip(
-                                    ratio,
-                                    1.0 - config["CLIP_EPS"],
-                                    1.0 + config["CLIP_EPS"],
-                                )
-                                * gae
+                            jnp.clip(
+                                ratio,
+                                1.0 - config["CLIP_EPS"],
+                                1.0 + config["CLIP_EPS"],
+                            )
+                            * gae
                         )
                         loss_actor = -jnp.minimum(loss_actor1, loss_actor2)
                         loss_actor = loss_actor.mean()
                         entropy = pi.entropy().mean()
 
                         total_loss = (
-                                loss_actor
-                                + config["VF_COEF"] * value_loss
-                                - config["ENT_COEF"] * entropy
+                            loss_actor
+                            + config["VF_COEF"] * value_loss
+                            - config["ENT_COEF"] * entropy
                         )
                         return total_loss, (value_loss, loss_actor, entropy)
 
@@ -298,36 +330,101 @@ def make_train(config):
                         * config["NUM_STEPS"],
                     }
                 )
+
             metric["update_steps"] = update_steps
-            jax.experimental.io_callback(callback, None, metric)
+            jax.debug.callback(callback, metric)
             update_steps = update_steps + 1
             runner_state = (train_state, env_state, last_obs, last_done, rng)
             return (runner_state, update_steps), None
 
         rng, _rng = jax.random.split(rng)
-        runner_state = (train_state, env_state, obsv, jnp.zeros((config["NUM_ACTORS"]), dtype=bool), _rng)
+        runner_state = (
+            train_state,
+            env_state,
+            obsv,
+            jnp.zeros((config["NUM_ACTORS"]), dtype=bool),
+            _rng,
+        )
         runner_state, _ = jax.lax.scan(
             _update_step, (runner_state, 0), None, config["NUM_UPDATES"]
         )
+
         return {"runner_state": runner_state}
 
     return train
 
-@hydra.main(version_base=None, config_path="config", config_name="ippo_ff_hint_guess")
-def main(config):
-    config = OmegaConf.to_container(config) 
+
+def single_run(config):
 
     wandb.init(
         entity=config["ENTITY"],
         project=config["PROJECT"],
-        tags=["IPPO", "FF", config["ENV_NAME"]],
+        tags=["PPO", config["ENV_NAME"].upper(), f"jax_{jax.__version__}"],
+        name=f'ppo_{config["ENV_NAME"]}',
         config=config,
         mode=config["WANDB_MODE"],
     )
 
-    rng = jax.random.PRNGKey(50)
-    train_jit = jax.jit(make_train(config), device=jax.devices()[0])
-    out = train_jit(rng)
+    rng = jax.random.PRNGKey(config["SEED"])
+    rngs = jax.random.split(rng, config["NUM_SEEDS"])
+    train_vjit = jax.jit(jax.vmap(make_train(config)))
+    outs = jax.block_until_ready(train_vjit(rngs))
+
+
+def tune(default_config):
+    """Hyperparameter sweep with wandb."""
+    import copy
+
+    def wrapped_make_train():
+
+        wandb.init(project=default_config["PROJECT"])
+        # update the default params
+        config = copy.deepcopy(default_config)
+        for k, v in dict(wandb.config).items():
+            config[k] = v
+
+        print("running experiment with params:", config)
+
+        rng = jax.random.PRNGKey(config["SEED"])
+        rngs = jax.random.split(rng, config["NUM_SEEDS"])
+        train_vjit = jax.jit(jax.vmap(make_train(config)))
+        outs = jax.block_until_ready(train_vjit(rngs))
+
+    sweep_config = {
+        "name": "ppo_hint_guess",
+        "method": "random",
+        "metric": {
+            "name": "returns",
+            "goal": "maximize",
+        },
+        "parameters": {
+            "HIDDEN_SIZE": {"values": [32, 128, 256, 512]},
+            "NUM_ENVS": {"values": [32, 64, 128, 256]},
+            "LR": {"values": [0.001, 0.0005, 0.0001, 0.00005, 0.00001]},
+            "ANNEAL_LR": {"values": [True, False]},
+            "ACTIVATION": {"values": ["relu", "tanh"]},
+            "UPDATE_EPOCHS": {"values": [2, 4, 8]},
+            "NUM_MINIBATCHES": {"values": [2, 4, 8, 16]},
+            "GAMMA": {"values": [0.99, 0.9, 0.]},
+            "CLIP_EPS": {"values": [0.1, 0.2, 0.3]},
+            "ENT_COEF": {"values": [0.0001, 0.001, 0.01]},
+            "NUM_STEPS": {"values": [2, 4, 8, 16]},
+            "MAX_GRAD_NORM": {"values": [1., 0.5]},
+        },
+    }
+
+    wandb.login()
+    sweep_id = wandb.sweep(
+        sweep_config, entity=default_config["ENTITY"], project=default_config["PROJECT"]
+    )
+    wandb.agent(sweep_id, wrapped_make_train, count=300)
+
+
+@hydra.main(version_base=None, config_path="config", config_name="ippo_ff_hint_guess")
+def main(config):
+    config = OmegaConf.to_container(config)
+    #single_run(config)
+    tune(config)
 
 
 if __name__ == "__main__":
