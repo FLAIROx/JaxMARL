5bf7817f6e9024131f73b86b0d1dab1131c6fcdf
alex <amacrutherford@gmail.com>
2024-03-29 16:43:52 +0000

    tidy

--- start_wandb_sweep.py
+++ start_wandb_sweep.py
@@ -3,8 +3,6 @@ import yaml
 import wandb
 from os import system
 
-ACTIVATE_VENV = '. path_to_your_virtualenv/bin/activate'
-
 
 def tmux(command):
     system('tmux %s' % command)
@@ -12,9 +10,6 @@ def tmux(command):
 
 def tmux_shell(command):
     tmux('send-keys "%s" "C-m"' % command)
-
-# example: one tab with vim, other tab with two consoles (vertical split)
-# with virtualenvs on the project, and a third tab with the server running
 
 sweep_config = yaml.load(open("sweep_smax.yaml", "r"), Loader=yaml.FullLoader)
 print('sweep_config:', sweep_config)
@@ -49,31 +44,3 @@ for gpu in range(gpu_count):
         
         tmux_shell(command)
 
-# # vim in project
-# tmux('select-window -t 0')
-# tmux_shell('vim')
-# tmux('rename-window "vim"')
-
-# # console in project
-# tmux('new-window')
-# tmux('select-window -t 1')
-# tmux_shell('cd %s' % PROJECT_PATH)
-# tmux_shell(ACTIVATE_VENV)
-# tmux('rename-window "consola"')
-# # second console as split
-# tmux('split-window -v')
-# tmux('select-pane -t 1')
-# tmux_shell('cd %s' % PROJECT_PATH)
-# tmux_shell(ACTIVATE_VENV)
-# tmux('rename-window "consola"')
-
-# # local server
-# tmux('new-window')
-# tmux('select-window -t 2')
-# tmux_shell('cd %s' % PROJECT_PATH)
-# tmux_shell(ACTIVATE_VENV)
-# tmux_shell('python manage.py runserver')
-# tmux('rename-window "server"')
-
-# # go back to the first window
-# tmux('select-window -t 0')
