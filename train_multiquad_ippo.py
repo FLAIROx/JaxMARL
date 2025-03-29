a31e4a04c4e29942a6c3598f4133529635da3792
Viktor Lorentz <viktor.lorentz@t-online.de>
2025-03-29 20:24:11 +0100

    add 500mil ipp

--- train_multiquad_ippo.py
+++ train_multiquad_ippo.py
@@ -39,7 +39,7 @@ def main():
     config = {
         "ENV_NAME": "multiquad_2x4",
         "ENV_KWARGS": {},
-        "TOTAL_TIMESTEPS": 10_000_000,
+        "TOTAL_TIMESTEPS": 500_000_000,
         "NUM_ENVS": 2048,
         "NUM_STEPS": 4096,
         "NUM_MINIBATCHES": 8,
