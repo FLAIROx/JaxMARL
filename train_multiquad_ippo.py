5b6c8c5ba63c5704081223a5ca2dafcaac7a4f96
Viktor Lorentz <viktor.lorentz@t-online.de>
2025-03-29 18:23:48 +0100

    less

--- train_multiquad_ippo.py
+++ train_multiquad_ippo.py
@@ -57,7 +57,7 @@ def main():
     config = {
         "ENV_NAME": "multiquad_2x4",
         "ENV_KWARGS": {},
-        "TOTAL_TIMESTEPS": 500_000_000,
+        "TOTAL_TIMESTEPS": 100_000_000,
         "NUM_ENVS": 1024,
         "NUM_STEPS": 2048,
         "NUM_MINIBATCHES": 8,
