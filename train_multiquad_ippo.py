0986374ce5f5edb59750c232ee978ae66d2ff25e
Viktor Lorentz <viktor.lorentz@t-online.de>
2025-04-02 14:22:54 +0200

    simpler arch

--- train_multiquad_ippo.py
+++ train_multiquad_ippo.py
@@ -91,7 +91,7 @@ def main():
         "GAE_LAMBDA": 0.95,
         "SEED": 0,
         "ACTOR_ARCH": [128, 64, 64],
-        "CRITIC_ARCH": [128, 128, 128, 128],
+        "CRITIC_ARCH": [128, 128, 128],
         "DISABLE_JIT": False,
         "PROJECT": "single_quad_rl",
         "NAME": f"quad_marl_{int(time.time())}",
