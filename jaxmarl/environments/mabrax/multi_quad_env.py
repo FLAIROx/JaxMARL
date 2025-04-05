39015dce2e493c542bd8f3fffef02833b9bc7a72
Viktor Lorentz <viktor.lorentz@t-online.de>
2025-04-05 16:44:40 +0200

    add aligned vel

--- jaxmarl/environments/mabrax/multi_quad_env.py
+++ jaxmarl/environments/mabrax/multi_quad_env.py
@@ -534,8 +534,9 @@ class MultiQuadEnv(PipelineEnv):
     target_reward = no_zero_while_error * (vel_cap - zero_at_target)
     
     target_reward = jp.exp(0.5 * target_reward)
+    target_reward += aligned_vel
     target_reward *= jp.exp(-2.0 * jp.abs(dis))
-    target_reward *= aligned_vel
+    
 
 
     smooth_action_penalty = jp.mean(jp.abs(action - last_action) / self.max_thrust)
