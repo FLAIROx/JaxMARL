03ab10579bbb55244dc1407fb51718ea038b2d2d
Viktor Lorentz <viktor.lorentz@t-online.de>
2025-04-06 11:27:32 +0200

    low energy coef

--- train_multiquad_ippo.py
+++ train_multiquad_ippo.py
@@ -70,7 +70,7 @@ def main():
         "collision_penalty_coef": -1.0,
         "out_of_bounds_penalty_coef": -1.0,
         "smooth_action_coef": -1.0,
-        "action_energy_coef": -1.0,
+        "action_energy_coef": -0.1,
     }
     # Build configuration for IPPO training on multiquad_2x4
     config = {
