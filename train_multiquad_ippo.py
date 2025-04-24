3c75080c39b33d9bbe28c1fcfab38f81df448eab
Viktor Lorentz <viktor.lorentz@t-online.de>
2025-04-24 21:56:29 +0200

    try trace

--- train_multiquad_ippo.py
+++ train_multiquad_ippo.py
@@ -248,10 +248,10 @@ def main():
     render_video(rollout, env)
     
 
-    actor_onnx = to_onnx(actor, [(1, obs_shape)])
+    actor_onnx = to_onnx(lambda x : actor(x), [(1, obs_shape)])
     onnx.save_model(actor_onnx, "actor_policy.onnx")
     print("Exported ONNX model: actor_policy.onnx")
-    critic_onnx = to_onnx(critic, [(1, obs_shape)])
+    critic_onnx = to_onnx(lambda x : critic(x), [(1, obs_shape)])
     onnx.save_model(critic_onnx, "critic_value.onnx")
     print("Exported ONNX model: critic_value.onnx")
 
