cf5d18225e75d9ec3915b405b3a007dec973dae0
Viktor Lorentz <viktor.lorentz@t-online.de>
2025-04-25 21:25:44 +0200

    no smooth action

--- train_singlequad_ippo.py
+++ train_singlequad_ippo.py
@@ -114,8 +114,8 @@ def main():
         "linvel_quad_reward_coef": 1.0,
         "taut_reward_coef": 0.0,
         "collision_penalty_coef": -1.0,
-        "out_of_bounds_penalty_coef": -0.1,
-        "smooth_action_coef": -0.1,
+        "out_of_bounds_penalty_coef": -0.0,
+        "smooth_action_coef": -0.0,
         "action_energy_coef": 0.0,
     }
     # Build configuration for IPPO training on multiquad_2x4
