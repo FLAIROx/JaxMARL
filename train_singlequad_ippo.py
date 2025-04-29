3ac18c2aa4908bd236e2a92e4524e3daf2a5915a
Viktor Lorentz <viktor.lorentz@t-online.de>
2025-04-29 15:23:38 +0200

    correct env reward

--- train_singlequad_ippo.py
+++ train_singlequad_ippo.py
@@ -119,7 +119,7 @@ def main():
         "collision_penalty_coef": -1.0,
         "out_of_bounds_penalty_coef": 0.0,
         "smooth_action_coef": -1.0,
-        "action_energy_coef": -0.5,
+        "action_energy_coef": -0.8,
     }
     # Build configuration for IPPO training on multiquad_2x4
     config = {
