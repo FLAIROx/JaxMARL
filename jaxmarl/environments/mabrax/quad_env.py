2a616d53d336df6bef5d0574f1cc69cb3991c900
Viktor Lorentz <viktor.lorentz@t-online.de>
2025-04-29 15:26:44 +0200

    adjust vel reward

--- jaxmarl/environments/mabrax/quad_env.py
+++ jaxmarl/environments/mabrax/quad_env.py
@@ -515,7 +515,7 @@ class QuadEnv(PipelineEnv):
     ang_vel_reward = (0.5 + 3 * er(dis, 20)) * (er(jp.linalg.norm(ang_vel_q1)))
     linvel_q1 = quad1_obs[12:15] 
 
-    linvel_quad_reward =  (0.5 + 6 * er(dis, 20)) * (er(jp.linalg.norm(linvel_q1)) )
+    linvel_quad_reward =  (0.5 + 6 * er(dis, 40)) * (er(jp.linalg.norm(linvel_q1)) )
 
     # Velocity alignment.
     target_dir  = pos_error / (dis + 1e-6)
