1dde6b46fceaaa75acb1090c73ca8e93da295f2e
Viktor Lorentz <viktor.lorentz@t-online.de>
2025-05-04 18:36:02 +0200

    250hz no noise

--- train_singlequad_ippo.py
+++ train_singlequad_ippo.py
@@ -126,8 +126,8 @@ def main():
         "ENV_NAME": "quad_1x4",
         "ENV_KWARGS": {
             "reward_coeffs": default_reward_coeffs,
-            "obs_noise": 1.0,
-            "act_noise": 0.1,
+            "obs_noise": 0.0,
+            "act_noise": 0.0,
             "max_thrust_range": 0.3,
             "episode_length": 4096,
             "policy_freq": 250,
