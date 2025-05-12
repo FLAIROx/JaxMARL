caa1d46d4a16b7ba42ae71bb639f10d395c43491
Viktor Lorentz <viktor.lorentz@t-online.de>
2025-05-12 15:35:55 +0200

    train at 500hz

--- train_singlequad_ippo.py
+++ train_singlequad_ippo.py
@@ -131,9 +131,9 @@ def main():
             "act_noise": 0.2,
             "max_thrust_range": 0.2,
             "episode_length": 3072,
-            "policy_freq": 250,
-            "tau_up": 0.15/4,   # T = 150ms => approx: tau =4T 
-            "tau_down":0.15/4,    #
+            "policy_freq": 500,
+            "tau_up": 0.05,   # T = 150ms => approx: tau =4T 
+            "tau_down":0.05,    #
             "sim_steps_per_action" : 2,
             "disturbance_chance": 0.001,      
         },
